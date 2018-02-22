f3279249bbd2740c041982fada9c8fe928cee861
Bing Steup <bingsteup@gmail.com>
2018-02-22 11:24:24 +0100

    Fix build Remove Sentry

--- App.js
+++ App.js
@@ -6,8 +6,6 @@ import store from './src/store'
 import { SafeAreaView } from 'react-navigation'
 import { ApolloProvider } from 'react-apollo'
 import {withRkTheme} from 'react-native-ui-kitten';
-import { Sentry } from 'react-native-sentry'
-Sentry.config('https://f63298c1df014e109ade398846b16058:b1d12fe3a70b4844974cdfde267ad593@sentry.io/288143').install()
 
 import {bootstrap} from './config/bootstrap';
 import { setupNotifications } from './src/services/notifications'
--- ios/DePunaise/AppDelegate.m
+++ ios/DePunaise/AppDelegate.m
@@ -11,11 +11,6 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#if __has_include(<React/RNSentry.h>)
-#import <React/RNSentry.h> // This is used for versions of react >= 0.40
-#else
-#import "RNSentry.h" // This is used for versions of react < 0.40
-#endif
 #import <Firebase.h>
 
 @implementation AppDelegate
@@ -29,8 +24,6 @@
                                                       moduleName:@"DePunaise"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-
-  [RNSentry installWithRootView:rootView];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
