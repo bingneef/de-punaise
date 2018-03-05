0f35c4943dafa575029c5dc94f949cee759330a2
Bing Steup <bingsteup@gmail.com>
2018-03-05 23:42:50 +0100

    Fix on Android for OpenSettings

--- android/app/src/main/java/com/snowflake/depunaise/MainApplication.java
+++ android/app/src/main/java/com/snowflake/depunaise/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.opensettings.OpenSettingsPackage;
 
 import io.invertase.firebase.RNFirebasePackage;
 // optional packages - add/remove as appropriate
@@ -41,8 +42,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-            new RNSentryPackage(MainApplication.this),
+        new RNSentryPackage(MainApplication.this),
         new RNMail(),
+        new OpenSettingsPackage(),
         new VectorIconsPackage(),
         new SplashScreenReactPackage(),
         new RNFirebasePackage(),
